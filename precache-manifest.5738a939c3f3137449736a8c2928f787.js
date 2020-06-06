413142cce364de6f4c72cf7676b4249ae7d67bb3
n.pankov <pankoff2000@gmail.com>
2020-06-06 15:52:35 +0200

    Updates

---
+++ precache-manifest.5738a939c3f3137449736a8c2928f787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4a58ed9c968cd82a9771a5da10ff25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2f8e1e03a2f727628f",
+    "url": "/static/js/2.65af4154.chunk.js"
+  },
+  {
+    "revision": "f863024c2d9d2f2bdd781cf3f534e630",
+    "url": "/static/js/2.65af4154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6203d44083a125accb7",
+    "url": "/static/js/main.75e2cd0c.chunk.js"
+  },
+  {
+    "revision": "b38f709691f005f4c585",
+    "url": "/static/js/runtime-main.e2af1984.js"
+  }
+]);
